dbcb1b0a6a82ab12c9e6700408a1f476e36e34bb
abdulkhalik8188 <125822798+abdulkhalik8188@users.noreply.github.com>
2023-03-18 11:54:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,14 +7,14 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", ""))
     API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "Maxx_Seaching_Bot")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "6076403137:AAEIZHpc-aIs_dfob6Q7oqLNMUYK5i8yqIg")
+    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "DD_mdisk_bot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001818361226")) 
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "Maxx_Seaching_Bot")
-    BOT_OWNER = int(os.getenv("BOT_OWNER", "2061410793"))
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001887632033")) 
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "DD_mdisk_bot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", "5941212132"))
 #    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "TR_TG_46")
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "Technical_rakesh_01")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "Kailash_ankal")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
     START_MSG = os.getenv("START_MSG", """**𝗛ᴇʏ 𝗕ᴜᴅᴅʏ! {}😃, 
 
@@ -25,24 +25,24 @@ class Config(object):
 𝗙ᴏʀ 𝗠ᴏʀᴇ 𝗜ɴꜰᴏ 𝗖ʟɪᴄᴋ 𝗢ɴ 𝗛ᴇʟᴘ ✅
 
 𝗠𝗔𝗗𝗘 𝗪𝗜𝗧𝗛 ❤️ 𝗕𝗬
-@Rakesh_Owner_Bot
+@Abdul88822
 
 """ ) 
-    START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/44e7072ec7355745a4ab5.jpg")
+    START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/9f1b1b151bda2c9ec3b25.jpg")
     HOME_TEXT = os.getenv("HOME_TEXT", """ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕
 
 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚃𝙷𝙴𝚁𝙴 𝚈𝙾𝚄 𝙻𝙸𝙽𝙺𝚂.
 
 𝙵𝙾𝚁 𝙼𝙾𝚁𝙴 𝙸𝙽𝙵𝙾 𝙲𝙻𝙸𝙲𝙺 𝙾𝙽 𝙷𝙴𝙻𝙿 ✅""" )
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001862060552")
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001836465539")
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001816512392"))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001896609847"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
-    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "Technical_rakesh_01")
+    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "pathan_h")
     FORCE_SUB = os.getenv("FORCE_SUB", "False")
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 180))
-    MDISK_API = os.getenv("MDISK_API", "fY4fnY9kkWCwfra5bYcE")
+    MDISK_API = os.getenv("MDISK_API", "dZul9OJxgbehif3vMtM4")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "31"))
     ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", """𝙸 𝙾𝙽𝙻𝚈 𝚂𝙷𝙰𝚁𝙴 𝚃𝙷𝙴 𝙿𝙾𝚂𝚃 𝙵𝚁𝙾𝙼 𝙿𝙴𝙾𝙿𝙻𝙴'𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻! 
 
@@ -50,7 +50,7 @@ class Config(object):
 
 𝙸 𝙽𝙾𝚃 𝚂𝚃𝙾𝚁𝙴 𝙰𝙽𝚈 𝙵𝙸𝙻𝙴 𝙾𝚁 𝚃𝙴𝚇𝚃 𝙸𝙽 𝙼𝚈 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴.
 
-𝙳𝙼 𝙵𝙾𝚁 𝙰𝙽𝚈 𝚀𝚄𝙴𝚁𝚈 @Rakesh_Owner_Bot 🤖""" )
+𝙳𝙼 𝙵𝙾𝚁 𝙰𝙽𝚈 𝚀𝚄𝙴𝚁𝚈 @Abdul88822 🤖""" )
     
     ABOUT_HELP_TEXT = """
 
@@ -76,7 +76,7 @@ class Config(object):
 
 ᴀɢᴀʀ ʙᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴀ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ɴʜɪ ᴋᴀʀ ʀᴀʜᴇ ʜᴀɪɴ ᴛᴏ ᴜɴʜᴇ ᴘᴇʀꜱᴏɴᴀʟ ᴍꜱɢ ᴋᴀʀᴇɴ.
 
-👉 @Rakesh_Owner_Bot
+👉 @Abdul88822
 
 """
 
