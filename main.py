755b3b9470316ce38cee412f02d60f25e201d882
abdulkhalik8188 <125822798+abdulkhalik8188@users.noreply.github.com>
2023-03-19 12:14:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ async def message_handler(event):
     '''
 
             newbutton = [Button.url('Help🙋',
-                                    f'http://t.me/Rakesh_Owner_Bot')]
+                                    f'https://t.me/pathan_h')]
 
             await txt.delete()
             result = await event.reply(answer, buttons=newbutton, link_preview=False)
@@ -132,7 +132,7 @@ async def message_handler(event):
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
 
         newbutton = [Button.url('How To Watch ❓',
-                                    f'https://t.me/dulink_file_open')]
+                                    f'https://t.me/pathan_h')]
 
         await txt.delete()
         await asyncio.sleep(0.5)
